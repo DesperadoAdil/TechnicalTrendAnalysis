79b7678b6ab73a391a5852b9bc4936bb658c9ffc
Adil <1298939544@qq.com>
2019-04-22 22:59:52 +0800

    feat: delete db.

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -3,6 +3,5 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config.from_object('config')
-db = SQLAlchemy(app, use_native_unicode="utf8")
 
-from app import views, models
+from app import views
